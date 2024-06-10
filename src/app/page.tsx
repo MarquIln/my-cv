e25e8137de9eeb7e0748b4b455bf74b2dd12a03e
Marcos <marcosraach.1@gmail.com>
2024-06-10 04:03:00 -0300

    refactor: Update page component with Chakra UI and project cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client'
-import { Linkedin } from "lucide-react"
-import { CvButton } from "./components/Button"
-import { CvCard } from "./components/Card"
+
+import { Flex, Text } from "@chakra-ui/react"
+import { Github, Linkedin } from "lucide-react"
+import { projects } from '../../projects.json'
+import { Button } from "./components/Button"
+import { Card } from "./components/Card"
+import { getCV } from "./utils/getCV"
+import { goToGithub } from "./utils/goToGithub"
+import { goToLinkedIn } from "./utils/goToLinkedIn"
 
 export default function Home() {
   function getGreeting() {
@@ -10,40 +16,53 @@ export default function Home() {
       return "Bom dia"
     } else if (currentTime >= 12 && currentTime < 18) {
       return "Boa tarde"
-    } else {
-      return "Boa noite"
     }
+    return "Boa noite"
   }
 
-  function downloadCV() {
-    window.open("https://drive.google.com/file/d/1CxAtjrd-yrFqwakL5IJCJCS1tMjXlOAg/view?usp=sharing", "_blank")
+  function goToProject(link: string) {
+    window.open(link, "_blank")
   }
 
-  function goToLinkedIn() {
-    window.open("https://www.linkedin.com/in/marcosraach/", "_blank")
-  }
+  const language = navigator.language
 
   return (
-    <main className="flex min-h-screen flex-col p-10 bg-zinc-800 text-zinc-100">
-      <div className="flex flex-col items-center justify-center gap-2">
-        <h1 className="text-4xl">Olá {getGreeting()}, tudo bem?</h1>
-        <h2 className="text-xl">Me chamo Marcos Raach e atualmente sou estudante de Engenharia de Software na PUCRS</h2>
-        <p>Estou buscando novos desafios e oportunidades para aprender e crescer profissionalmente na área</p>
-        <div className="flex gap-10">
-          <CvButton content="Download do Curriculo" onClick={downloadCV} />
-          <CvButton content="LinkedIn" onClick={goToLinkedIn} icon={<Linkedin />} />
-        </div>
-      </div>
-      <div>
-        <h1 className="text-xl text-center py-10">
-          Projetos em que participei:
-        </h1>
-        <div className="flex flex-col items-center justify-center gap-10 py-10">
-          <CvCard title="Comunidade Universitaria" description="a"/>
-          <CvCard title="Trivia Quiz" description="a"/>
-          <CvCard title="Guess CS:GO Skin" description="a"/>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col p-10 bg-slate-900 text-zinc-200">
+      <Flex className="flex-col gap-2">
+        <Text fontSize="xxx-large">Olá, {getGreeting()}, tudo bem?</Text>
+        <Text fontSize="xx-large">Vou me apresentar um pouco para você...</Text>
+        <Flex className="flex-col px-10">
+          <Text fontSize="larger">Me chamo Marcos Vinicius Raach, estou atualmente no 5º semestre de Engenharia de Software na PUC-RS,</Text>
+          <Text fontSize="larger">Sou uma pessoa apaixonada por tecnologia desde criança e após ingressar na faculdade, me apaixonei pela programação e desenvolvimento de software.</Text>
+          <Text fontSize="larger">Embora não tenha tido ainda nenhuma experiencia no mercado de trabalho, tenho me dedicado a desenvolver projetos pessoais e projetos da faculdade para adquirir conhecimentos e o mais rapido possivel ingressar como estagiario em alguma empresa.</Text>
+        </Flex>
+        <Flex className="px-20 flex-col">
+          <Text className="px-15" fontSize="larger">Se quiser saber mais sobre mim, acesse meu curriculo ou entre em contato comigo pelas redes sociais abaixo: </Text>
+          <Flex className="py-5 gap-5 justify-center">
+            <Button content="Curriculum" onClick={() => getCV(language)} />
+            <Button content="LinkedIn" onClick={() => goToLinkedIn()} icon={<Linkedin />} />
+            <Button content="GitHub" onClick={() => goToGithub()} icon={<Github />} />
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex className="flex-col justify-center">
+        <Text fontSize="larger">
+          Aqui estão alguns dos projetos pessoais e projetos da faculdade que desenvolvi até agora:
+        </Text>
+        <Flex className="flex gap-5">
+          {projects.map((project, index) => (
+            <Card
+              key={index}
+              title={project.name}
+              image={project.image}
+              description={project.description}
+              onClick={() => goToProject(project.link)}
+              buttonContent={project.buttonContent}
+            />
+          ))}
+        </Flex>
+
+      </Flex>
     </main>
   );
 }
