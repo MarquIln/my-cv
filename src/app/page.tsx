b18724d2c3bb368fedd621d0d9b27807c0bac488
Marcos <marcosraach.1@gmail.com>
2024-06-10 15:28:34 -0300

    refactor: Update page component with Chakra UI, add repository links, and update project cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,29 @@ import { projects } from '../../projects.json'
 import { Button } from "./components/Button"
 import { Card } from "./components/Card"
 import { getCV } from "./utils/getCV"
-import { goToGithub } from "./utils/goToGithub"
-import { goToLinkedIn } from "./utils/goToLinkedIn"
+import { goToGithub, goToProject } from "./utils/openWindow"
+import { goToLinkedIn } from "./utils/openWindow"
+import { getGreeting } from "./utils/getGreeting"
+import { ProfileCard } from "./components/ProfileCard"
 
 export default function Home() {
-  function getGreeting() {
-    const currentTime = new Date().getHours()
-    if (currentTime >= 5 && currentTime < 12) {
-      return "Bom dia"
-    } else if (currentTime >= 12 && currentTime < 18) {
-      return "Boa tarde"
-    }
-    return "Boa noite"
-  }
-
-  function goToProject(link: string) {
-    window.open(link, "_blank")
-  }
-
   const language = navigator.language
+  const thisProjectLink = "https://github.com/MarquIln/my-cv"
 
   return (
-    <main className="flex min-h-screen flex-col p-10 bg-slate-900 text-zinc-200">
-      <Flex className="flex-col gap-2">
-        <Text fontSize="xxx-large">Olá, {getGreeting()}, tudo bem?</Text>
+    <main className="flex min-h-screen flex-col bg-slate-900 text-zinc-300">
+      <Flex className="flex-col gap-2 p-10">
+        <Flex className="justify-between">
+          <Text fontSize="xxx-large">Olá, {getGreeting()}, tudo bem?</Text>
+          <Button content="Repositorio do Projeto" onClick={() => goToProject(thisProjectLink)}/>
+        </Flex>
         <Text fontSize="xx-large">Vou me apresentar um pouco para você...</Text>
         <Flex className="flex-col px-10">
-          <Text fontSize="larger">Me chamo Marcos Vinicius Raach, estou atualmente no 5º semestre de Engenharia de Software na PUC-RS,</Text>
-          <Text fontSize="larger">Sou uma pessoa apaixonada por tecnologia desde criança e após ingressar na faculdade, me apaixonei pela programação e desenvolvimento de software.</Text>
-          <Text fontSize="larger">Embora não tenha tido ainda nenhuma experiencia no mercado de trabalho, tenho me dedicado a desenvolver projetos pessoais e projetos da faculdade para adquirir conhecimentos e o mais rapido possivel ingressar como estagiario em alguma empresa.</Text>
+          <ProfileCard
+            description="Me chamo Marcos Vinicius Raach, estou atualmente no 5º semestre de Engenharia de Software na PUC-RS, sou uma pessoa apaixonada por tecnologia desde criança e após ingressar na faculdade, me apaixonei pela programação e desenvolvimento de software. Embora não tenha tido ainda nenhuma experiencia no mercado de trabalho, tenho me dedicado a desenvolver projetos pessoais e projetos da faculdade para adquirir conhecimentos e o mais rapido possivel ingressar como estagiario em alguma empresa."
+            title="Sobre mim"
+            image={"https://github.com/Marquiln.png"}
+          />
         </Flex>
         <Flex className="px-20 flex-col">
           <Text className="px-15" fontSize="larger">Se quiser saber mais sobre mim, acesse meu curriculo ou entre em contato comigo pelas redes sociais abaixo: </Text>
@@ -45,11 +39,11 @@ export default function Home() {
           </Flex>
         </Flex>
       </Flex>
-      <Flex className="flex-col justify-center">
-        <Text fontSize="larger">
+      <Flex className="flex-col p-10">
+        <Text fontSize="larger" className="py-5">
           Aqui estão alguns dos projetos pessoais e projetos da faculdade que desenvolvi até agora:
         </Text>
-        <Flex className="flex gap-5">
+        <Flex className="gap-5">
           {projects.map((project, index) => (
             <Card
               key={index}
@@ -58,10 +52,10 @@ export default function Home() {
               description={project.description}
               onClick={() => goToProject(project.link)}
               buttonContent={project.buttonContent}
+              repositoryLink={project.repositoryLink}
             />
           ))}
         </Flex>
-
       </Flex>
     </main>
   );
